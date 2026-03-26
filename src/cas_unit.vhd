8b1d40b79f57064d25237c404a14ced7ade062e3
Costas Giotis <210527419+costas-g@users.noreply.github.com>
2026-03-26 17:18:24 +0200

    Design the CAS unit

---
+++ src/cas_unit.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity cas_unit is
+    generic (
+        WIDTH : integer := 9
+    );
+    port (
+        a   : in  std_logic_vector(WIDTH-1 downto 0); -- 9-bit signed input
+        b   : in  std_logic_vector(WIDTH-1 downto 0); -- 9-bit signed input (supposed to be positive)
+        sub : in  std_logic;                          -- Sub control bit (active high)
+        res : out std_logic_vector(WIDTH-1 downto 0)  -- 9-bit signed ouput
+    );
+end entity cas_unit;
+
+architecture behavioral of cas_unit is
+begin
+    process(a, b, sub)
+        variable a_signed : signed(WIDTH-1 downto 0);
+        variable b_signed : signed(WIDTH-1 downto 0);
+    begin
+        a_signed := signed(a);
+        b_signed := signed(b);
+        
+        if sub = '1' then
+            res <= std_logic_vector(a_signed - b_signed);
+        else
+            res <= std_logic_vector(a_signed + b_signed);
+        end if;
+    end process;
+end architecture behavioral;
